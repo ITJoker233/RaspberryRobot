1b81a0d201b4d75b339dde9a9a9b0baf6628bbe7
ITJoker233 <649258008@qq.com>
2022-02-24 17:18:33 +0800

    Update app.py [add output logs]

--- app.py
+++ app.py
@@ -3,9 +3,22 @@ from Robot import signal_handler
 from config import RESOURCE_FILE, DETECT_DING, DETECT_DONG, MODELS, SENSITIVITY,TEMP_PATH
 from AI import AI
 from Robot import Brian
+from logging import getLogger
 
 if not os.path.exists(TEMP_PATH): os.mkdir(TEMP_PATH)
-
+logger = getLogger(__name__)
+logger.info(
+    '''
+██████╗  █████╗ ███████╗██████╗     ██████╗  ██████╗ ██████╗  ██████╗ ████████╗
+██╔══██╗██╔══██╗██╔════╝██╔══██╗    ██╔══██╗██╔═══██╗██╔══██╗██╔═══██╗╚══██╔══╝
+██████╔╝███████║███████╗██████╔╝    ██████╔╝██║   ██║██████╔╝██║   ██║   ██║   
+██╔══██╗██╔══██║╚════██║██╔═══╝     ██╔══██╗██║   ██║██╔══██╗██║   ██║   ██║   
+██║  ██║██║  ██║███████║██║         ██║  ██║╚██████╔╝██████╔╝╚██████╔╝   ██║   
+╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝         ╚═╝  ╚═╝ ╚═════╝ ╚═════╝  ╚═════╝    ╚═╝   
+Author:ITJoker233
+Latest Times:2022.02.24
+    '''
+)
 signal.signal(signal.SIGINT, signal_handler)
 
 brian = Brian(
@@ -18,6 +31,6 @@ brian = Brian(
 )
 
 brian.middleware.add(AI);
-    
+
 brian.start()
 brian.terminate()
